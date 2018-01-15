f093a597baf5a7659233e9f04f496c24241d301c
J. Kristoffer Legind <klegind@gmail.com>
2018-01-15 15:09:40 +0100

    double backslash

--- BIDdatasets.sql
+++ BIDdatasets.sql
@@ -5,6 +5,6 @@ SELECT (xpath('./dataset/project/@id', t1.eml))[1]::text AS eml, t1.dk, t1.datas
   JOIN organization o ON node.key = o.endorsing_node_key
   JOIN dataset d ON o.key = d.publishing_organization_key
   JOIN metadata m ON d.key = m.dataset_key WHERE
-  d.deleted IS NULL AND encode(m.content, 'escape') !~ '^\357\273\277|^\377\376'
+  d.deleted IS NULL AND encode(m.content, 'escape') !~ '^\\357\\273\\277|^\\377\\376'
 )t1
 WHERE trim((xpath('./dataset/project/@id', t1.eml))[1]::text) LIKE '%BID%'
